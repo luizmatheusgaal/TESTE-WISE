45cc1a0b21566bb8efa96084bb8a0f179ad18ae1
Luiz Matheus G de Almeida <luizmatheusga@gmail.com>
2026-03-25 12:56:27 -0300

    feat: add db settings

---
+++ backend/src/db.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+from psycopg2.pool import SimpleConnectionPool
+from src.settings import settings
+
+
+pool = SimpleConnectionPool(1, 10, dsn=settings.dsn)
+
+
+@contextmanager
+def get_connection():
+    connection = pool.getconn()
+    try:
+        yield connection
+        connection.commit()
+
+    except Exception:
+        connection.rollback()
+        raise
+
+    finally:
+        pool.putconn(connection)
