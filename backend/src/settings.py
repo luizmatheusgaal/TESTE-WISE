c1cc28d5f5777f2a086082c476703fb3d7e10140
Luiz Matheus G de Almeida <luizmatheusga@gmail.com>
2026-03-25 01:35:40 +0000

    feat: add db settings

---
+++ backend/src/settings.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import (
+    BaseSettings,
+    DotEnvSettingsSource,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+)
+
+ROOT_ENV_FILE = Path(__file__).resolve().parents[2] / ".env"
+
+
+class Settings(BaseSettings):
+    db_host: str = Field(...)
+    db_port: int = Field(...)
+    db_name: str = Field(...)
+    db_user: str = Field(...)
+    db_password: str = Field(...)
+
+    model_config = SettingsConfigDict(
+        env_file=ROOT_ENV_FILE,
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls,
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: DotEnvSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ):
+        return (dotenv_settings,)
+
+    @property
+    def dsn(self) -> str:
+        return (
+            f"dbname={self.db_name} user={self.db_user} password={self.db_password} "
+            f"host={self.db_host} port={self.db_port}"
+        )
+
+
+settings = Settings()
